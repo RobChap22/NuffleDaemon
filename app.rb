21f6f919fdbd2fb1b279d9b85e8e43f8e86fc908
Rob Chapman <autosexual@gmail.com>
2021-02-04 10:04:35 +0900

    initial neatening

--- app.rb
+++ app.rb
@@ -6,9 +6,6 @@ require "uri"
 require "tempfile"
 require "line/bot"
 
-require_relative "ibm_watson"
-require_relative "weather_api"
-require_relative "tokyo_events_api"
 
 def client
   @client ||= Line::Bot::Client.new { |config|
